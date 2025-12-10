fbdd5811227f4eddc48b8da43126eaf345ae9890
bigboyconst <constantinbrouillard4@gmail.com>
2025-12-10 14:30:11 -0500

    Extend nob.c to allow multiple source files for each target

--- nob.c
+++ nob.c
@@ -6,29 +6,47 @@
 #define SRC_DIR "src/"
 #define THIRDPARTY_DIR "src/thirdparty/"
 
+// This is so fucking cursed bro
+typedef struct {
+	const char*** items;
+	size_t count;
+	size_t capacity;
+} Sources;
+
+Sources sources = {0};
+
 const char* targets[] = {
 	"cudatest"
 };
 
 Cmd cmd = {0};
 
-// TODO: Extend to take in multiple source files (a single one isn't really my style)
-void nvcc(const char* target, const char* extension) {
+void init_sources() {
+	// cudatest
+	const char* cudatest_srcs[] = { "cudatest.cu" };
+	da_append(&sources, cudatest_srcs);
+}
+
+void nvcc(const char* target, const char** sources) {
 	const char* bin_name = temp_sprintf(BUILD_DIR"%s", target);
-	const char* src_name = temp_sprintf(SRC_DIR"%s/%s%s", target, target, extension);
 	cmd_append(&cmd, "nvcc", 
 		"-o", bin_name,
-		"-I"THIRDPARTY_DIR,
-		src_name
+		"-I"THIRDPARTY_DIR
 	);
+	for (size_t i = 0; i < NOB_ARRAY_LEN(sources); i++) {
+		const char* src_name = temp_sprintf(SRC_DIR"%s/%s", target, sources[i]);
+		cmd_append(&cmd, src_name);
+	}
 }
 
 int main(int argc, char** argv) {
 	NOB_GO_REBUILD_URSELF(argc, argv);
 
+	init_sources();
+
 	if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
 
-	nvcc(targets[0], ".cu");
+	nvcc(targets[0], sources.items[0]);
 
 	if (!cmd_run_sync_and_reset(&cmd)) return 1;
 
